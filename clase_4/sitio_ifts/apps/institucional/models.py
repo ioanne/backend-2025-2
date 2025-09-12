6a57cccd70caf85642b6fd17288da846d0574305
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2025-09-12 20:21:18 -0300

    Agregamos cambios institucionales.

--- clase_4/sitio_ifts/apps/institucional/models.py
+++ clase_4/sitio_ifts/apps/institucional/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Institucional(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    mision = models.TextField()
+    vision = models.TextField()
+    historia = models.TextField()
+
+    def __str__(self):
+        return self.nombre
