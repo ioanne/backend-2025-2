6ff80f1493900eff62e8cf8698e689c6f9e1202d
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2025-09-12 20:20:18 -0300

    Agregamos cambios en modelo de cursada.

--- clase_4/sitio_ifts/apps/cursada/models.py
+++ clase_4/sitio_ifts/apps/cursada/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 
 class Cursada(models.Model):
+    año = models.IntegerField(null=True, blank=True)
+    cuatrimestre = models.IntegerField(null=True, blank=True)
     materia = models.ForeignKey(
         'materia.Materia', on_delete=models.CASCADE, null=True, related_name='cursadas'
     )
     profesor = models.ForeignKey(
         'profesor.Profesor', on_delete=models.CASCADE, null=True, related_name='cursadas'
     )
-    año = models.IntegerField(null=True, blank=True)
-    cuatrimestre = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.materia.nombre if self.materia else 'Sin materia'} - {self.profesor.nombre if self.profesor else 'Sin profesor'} - {self.año} - {self.cuatrimestre}"
+    
